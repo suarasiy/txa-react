2bff75a17f3d2528ba99bbbf99d9addccca1be4b
suarasiy <sidiqindrajatiyusuf@gmail.com>
2021-01-31 02:27:29 +0800

    Remove default files

---
+++ src/props/Child.tsx
@@ -0,0 +1,5 @@
+export const Child = () => {
+  return (
+    <div>Hi There!</div>
+  )
+};
---
+++ src/props/Parent.tsx
@@ -0,0 +1,9 @@
+import { Child } from './Child';
+
+const Parent = () => {
+  return (
+    <Child />
+  )
+}
+
+export default Parent;
